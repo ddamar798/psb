111b18a78e030121437c1089dcf676559da88ebd
daamar <damarbudiprasetyo714@gmail.com>
2025-06-13 13:10:29 +0700

    Update cetak-bukti.php

--- cetak-bukti.php
+++ cetak-bukti.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
+	<meta charset="utf-8"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>PSB Online</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
